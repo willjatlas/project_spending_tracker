6b9d7a0e5324b20d93ee28398e0ac9d71e13a59a
William Anderson <willjatlas@googlemail.com>
2020-11-28 15:11:37 +0000

    added tests to add/subtract from wallet

--- tests/test_user.py
+++ tests/test_user.py
@@ -12,3 +12,17 @@ class TestUser(unittest.TestCase):
 
     def test_user_has_last_name(self):
         self.assertEqual("Nadir", self.user2.last_name)
+
+    def test_user_has_email(self):
+        self.assertEqual("troybarnes@taaitm.com", self.user.email)
+    
+    def test_user_has_wallet(self):
+        self.assertEqual(250.00, self.user2.wallet)
+    
+    def test_user_is_charged(self):
+        self.user.charge_wallet(19.50)
+        self.assertEqual(140.50, self.user.wallet)
+
+    def test_user_can_add_funds(self):
+        self.user2.add_to_wallet(14.20)
+        self.assertEqual(264.20, self.user2.wallet)
