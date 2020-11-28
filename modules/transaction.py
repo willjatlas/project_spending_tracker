ec0d6d31817b291b937851b997970e1effa14fce
William Anderson <willjatlas@googlemail.com>
2020-11-28 17:04:29 +0000

    final transaction tests added

--- modules/transaction.py
+++ modules/transaction.py
@@ -1,9 +1,14 @@
 class Transaction:
 
-    def __init__(self, date, time, amount, id = None):
+    def __init__(self, user, date, time, merchant, amount, tag, id = None):
+        self.user = user
         self.date = date
         self.time = time
+        self.merchant = merchant
         self.amount = amount
+        self.tag = tag
         self.id = id
+
+
 
     
--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,9 +1,15 @@
 import unittest
 from modules.transaction import Transaction
+from modules.user import User
+from modules.tag import Tag
+from modules.merchant import Merchant
 
 class testTransaction(unittest.TestCase):
 
     def setUp(self):
+        self.user = User("Ben", "Chang", "changsyaboi@taaitm.com", 145.60)
+        self.merchant = Merchant("Scotmid", "General grocieries and provider of snax")
+        self.tag = Tag("Snacks")
         self.transaction = Transaction("11/05/20", "19:40", 6.50)
 
     def test_transaction_has_date(self):
@@ -14,3 +20,13 @@ class testTransaction(unittest.TestCase):
 
     def test_transaction_has_amount(self):
         self.assertEqual(6.50, self.transaction.amount)
+
+    def test_transaction_has_user(self):
+        self.assertEqual(self.user, self.transaction.user)
+
+    def test_transaction_has_merchant(self):
+        self.assertEqual(self.merchant, self.transaction.merchant)
+    
+    def test_transaction_has_tag(self):
+        self.assertEqual(6.50, self.transaction.tag)
+        
