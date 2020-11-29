dfdb1334dbe60fd375887bde1e6d55a9868bd80a
William Anderson <willjatlas@googlemail.com>
2020-11-29 15:45:13 +0000

    added table info in console.py

--- console.py
+++ console.py
@@ -5,3 +5,38 @@ from modules.transaction import Transaction
 from modules.merchant import Merchant
 from modules.tag import Tag
 
+import repositories.user_repository as user_repository
+import repositories.tag_repository as tag_repository
+import repositories.merchant_repository as merchant_repository
+import repositories.transaction_repository as transaction_repository
+
+user_repository.delete_all()
+tag_repository.delete_all()
+merchant_repository.delete_all()
+transaction_repository.delete_all()
+
+user1 = User("Troy", "Barnes", "troybarnes@taaitm.com", 160.00)
+user_repository.save(user1)
+
+user2 = User("Abed", "Nadir", "abednadir@taaitm.com", 250.00)
+user_repository.save(user2)
+
+user3 = User("Ben", "Chang", "changsyaboi@taaitm.com", 145.60)
+user_repository.save(user3)
+
+tag1 = Tag("Groceries")
+tag2 = Tag("Business")
+tag3 = Tag("Snacks")
+tag4 = Tag("Leisure")
+
+merchant1 = Merchant("Scotmid", "General grocieries and provider of snax.")
+merchant2 = Merchant("Netflix", "Online streaming platform for movies and tv shows.")
+merchant3 = Merchant("Apple" , "Electronic hardware and software provider.")
+
+transaction1 = Transaction(user1, "11/05/20", "19:40", merchant1, 6.50, tag3)
+transaction1 = Transaction(user2, "10/08/20", "14:32", merchant2, 9.99, tag4)
+transaction1 = Transaction(user3, "05/06/20", "12:20", merchant3, 59.00, tag2)
+transaction1 = Transaction(user1, "13/07/20", "18:55", merchant1, 6.50, tag1)
+transaction1 = Transaction(user2, "17/08/20", "07:29", merchant3, 19.99, tag4)
+
+pdb.set_trace()
