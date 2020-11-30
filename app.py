d7d59db32a374325ae534320d23576c3cb7fb3e7
William Anderson <willjatlas@googlemail.com>
2020-11-30 21:11:06 +0000

    merchants edit and delete finished, added css and html formatiing

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 from controllers.transactions_controller import transactions_blueprint
 from controllers.tags_controller import tags_blueprint
+from controllers.merchants_controller import merchants_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(transactions_blueprint)
 app.register_blueprint(tags_blueprint)
+app.register_blueprint(merchants_blueprint)
 
 
 @app.route('/')
--- controllers/merchants_controller.py
+++ controllers/merchants_controller.py
@@ -1,9 +1,48 @@
 from flask import Blueprint, Flask, redirect, render_template, request
+from modules.merchant import Merchant
+
 import repositories.merchant_repository as merchant_repository
 
 merchants_blueprint = Blueprint("merchants", __name__)
 
-# @merchants_blueprint.route("/merchants")
-# def merchants():
-#     merchants = merchant_repository.select_all()
-#     return render_template("")
+# Index
+@merchants_blueprint.route("/merchants")
+def merchants():
+    merchants = merchant_repository.select_all()
+    return render_template("merchants/index.html", merchants = merchants)
+
+# New
+@merchants_blueprint.route("/merchants/new")
+def new_merchant():
+    return render_template("merchants/new.html")
+
+# Create
+@merchants_blueprint.route("/merchants", methods=["POST"])
+def create_merchant():
+    name = request.form["name"]
+    description = request.form["description"]
+    merchant = Merchant(name, description)
+    merchant_repository.save(merchant)
+    return redirect("/merchants")
+
+# Delete
+@merchants_blueprint.route("/merchants/<id>/delete")
+def delete(id):
+    merchant_repository.delete(id)
+    return redirect("/merchants")
+
+# Edit 
+@merchants_blueprint.route("/merchants/<id>/edit")
+def edit(id):
+    merchant = merchant_repository.select(id)
+    return render_template("merchants/edit.html", merchant = merchant)
+
+# Update
+@merchants_blueprint.route("/merchants/<id>", methods=["POST"])
+def update_merchant(id):
+    name = request.form["name"]
+    description = request.form["description"]
+    merchant = Merchant(name, description, id)
+    merchant_repository.update(merchant)
+    return redirect("/merchants")
+
